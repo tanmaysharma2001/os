bd86ed264f74b9a85801b2062c960df508cc41d2
Tanmay Sharma <sharmatanmay617@gmail.com>
2022-09-20 22:59:01 +0300

    Fix ex4

--- week03/ex4.c
+++ week03/ex4.c
@@ -38,22 +38,6 @@ void *mulDouble(const void *a, const void *b)
     return (void *)returnPointer;
 }
 
-void *meanInt(const void *a, const void *b)
-{
-
-    //<WRITE YOUR CODE HERE>;
-}
-
-void *meanDouble(const void *a, const void *b)
-{
-
-    //<WRITE YOUR CODE HERE>;
-}
-
-void *mean()
-{
-}
-
 void *aggregate(void *base, size_t size, int n, void *initial_value, void *(*opr)(const void *, const void *))
 {
 
@@ -63,40 +47,24 @@ void *aggregate(void *base, size_t size, int n, void *initial_value, void *(*opr
     { // base is a pointer to an integer
 
         //<WRITE YOUR CODE HERE>;
-        if (opr != mean)
+        int initialValue = *((int *)initial_value);
+        // as we need the actual adress instead of the value.
+        output = &initialValue;
+
+        for (int i = 0; i < n; i++)
         {
-            int initialValue = *((int *)initial_value);
-            output = &initialValue;
-            for (int i = 0; i < n; i++)
-            {
-                output = opr(output, ((int *)base + i));
-            }
-        }
-        else
-        {
-            output = aggregate(base, size, n, initial_value, &addInt);
-            int rett = *((int *)output) / n;
-            output = &rett;
+            output = opr(output, ((int *)base + i));
         }
     }
     else
     { // base is a pointer to a double
 
         //<WRITE YOUR CODE HERE>;
-        if (opr != mean)
+        double initialValue = *((double *)initial_value);
+        output = &initialValue;
+        for (int i = 0; i < n; i++)
         {
-            double initialValue = *((double *)initial_value);
-            output = &initialValue;
-            for (int i = 0; i < n; i++)
-            {
-                output = opr(output, ((double *)base + i));
-            }
-        }
-        else
-        {
-            output = aggregate(base, size, n, initial_value, &addDouble);
-            double ret = *((double *)output) / n;
-            output = &ret;
+            output = opr(output, ((double *)base + i));
         }
     }
 
@@ -114,12 +82,12 @@ int main()
     //<WRITE YOUR CODE HERE>;
     for (int i = 0; i < n; i++)
     {
-        *(ints + i) = i * 1;
+        *(ints + i) = i + 1;
     }
 
     for (int i = 0; i < n; i++)
     {
-        *(doubles + i) = i * 2.0;
+        *(doubles + i) = i + 2.0;
     }
 
     // initial values
@@ -138,13 +106,13 @@ int main()
 
     // Addition
 
-    int *result1a = aggregate(ints, sizeof (int), n, addIntInitialPointer, &addInt);
+    int *result1a = aggregate(ints, sizeof(int), n, addIntInitialPointer, &addInt);
 
     //<WRITE YOUR CODE HERE>;
 
     printf("%d\n", *result1a);
 
-    double *result2a = aggregate(doubles, sizeof (double), n, addDoubleInitialPointer, &addDouble);
+    double *result2a = aggregate(doubles, sizeof(double), n, addDoubleInitialPointer, &addDouble);
 
     //<WRITE YOUR CODE HERE>;
 
@@ -152,13 +120,13 @@ int main()
 
     // Multiplication
 
-    int *result1m = aggregate(ints, sizeof (int), n, mulIntInitialPointer, &mulInt);
+    int *result1m = aggregate(ints, sizeof(int), n, mulIntInitialPointer, &mulInt);
 
     //<WRITE YOUR CODE HERE>;
 
     printf("%d\n", *result1m);
 
-    double *result2m = aggregate(doubles, sizeof (double), n, mulDoubleInitialPointer, &mulDouble);
+    double *result2m = aggregate(doubles, sizeof(double), n, mulDoubleInitialPointer, &mulDouble);
 
     //<WRITE YOUR CODE HERE>;
 
@@ -166,17 +134,17 @@ int main()
 
     // Mean
 
-    int *result1mean = aggregate(ints, sizeof (int), n, addIntInitialPointer, &mean);
+    int *result1mean = aggregate(ints, sizeof(int), n, addIntInitialPointer, &addInt);
 
     //<WRITE YOUR CODE HERE>;
 
-    printf("%d\n", *result1mean);
+    printf("%d\n", (*result1mean / n));
 
-    double *result2mean = aggregate(doubles, sizeof (double), n, addDoubleInitialPointer, &mean);
+    double *result2mean = aggregate(doubles, sizeof(double), n, addDoubleInitialPointer, &addDouble);
 
     //<WRITE YOUR CODE HERE>;
 
-    printf("%f\n", *result2mean);
+    printf("%f\n", (*result2mean / n));
 
     // free the pointers
     //<WRITE YOUR CODE HERE>;
