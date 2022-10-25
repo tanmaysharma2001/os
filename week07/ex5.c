521bbe03fa40a1949f23e715e2374cf3fc45074a
tanmaysharma2001 <sharmatanmay617@gmail.com>
2022-10-25 23:43:45 +0300

    exercise 5 modified

--- week07/ex5.c
+++ week07/ex5.c
@@ -6,7 +6,7 @@ int main()
 	char **s = (char **)malloc(sizeof(char *));
 	char foo[] = "Hello World";
 	*s = foo;
-	printf("s is %p\n", s);
+	printf("s is %p\n", s); // s is a pointer 
 	s[0] = foo;
 	printf("s[0] is %s\n", s[0]);
 
