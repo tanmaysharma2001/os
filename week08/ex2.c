8871d85856fe5c76e82208cf96e32fe6e01237e2
tanmaysharma2001 <sharmatanmay617@gmail.com>
2022-11-01 22:23:32 +0300

    Week 08 done

--- week08/ex2.c
+++ week08/ex2.c
@@ -1,11 +1,16 @@
 // ex2
-// In the operating system, si and so fields are the indication that
+// In the operating system, 'si' and 'so' fields are the indication that
 // the main memory has no space left, i.e., full and now the swap space will be used
 // for storing the programs. But as observed from the command's result,
 // the fields have values 0, indicates that the main memory is not currently full 
 // (during the 10 seconds of the program) hence they are 0 and swapping will not be done.
 
 // ex3
+// The 'top' command shows the current high priority process running on the system. 
+// While the ex2 executable running in the background, it allocates 10 MBs of memory every second.
+// Meanwhile, the top command (updates every 3 seconds), shows it two times with memory usage of 0.3
+// and 1.5 representing here our working of ex2 file, and the memory allocation that is being carried
+// out in the process. 
 
 #include <stdio.h>
 #include <stdlib.h>
--- week08/ex3.sh
+++ week08/ex3.sh
@@ -1,3 +1,3 @@
 gcc ex2.c -o ex2
 ./ex2 &
-vmstat 1 > ex2.txt
+top > ex3.txt
